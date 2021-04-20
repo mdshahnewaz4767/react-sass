e28f3f6861f82ac9d64d640a45074b1740fef70e
shahnewaz601 <shahnewaz601@gmail.com>
2021-04-21 00:10:13 +0600

    implement Shared style in sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import './App.scss';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Home></Home>
     </div>
   );
 }
